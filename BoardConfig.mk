92f7972aac17242855148fc23b4d70cabefdead8
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-17 19:59:06 -0500

    inc: set screen dimensions to build philz
    Change-Id: I862c4ef75fcf3805062bd2dbed970a510dc7d738

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,3 +75,6 @@ BOARD_CACHE_FILESYSTEM := auto
 
 # Make sure vold formats the proper sdcard partition
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
